a2204958eb8dd5f200d6700c31060b8390318823
72fcosta <inbox5372@gmail.com>
2021-05-07 05:51:23 -0300

    ajustes

--- src/filters/datePretty.js
+++ src/filters/datePretty.js
@@ -1,7 +1,14 @@
 const datePretty = date => {
    if (!date) return ''
 
-   const datetime = new Date(date)
+   const isTimestampObject = !!date.nanoseconds && !!date.seconds
+   let datetime
+   if (isTimestampObject) {
+      datetime = new Date(date.seconds * 1000)
+   } else {
+      datetime = new Date(date)
+   }
+
    const dateOptions = { year: '2-digit', month: '2-digit', day: '2-digit' }
 
    return `${datetime.toLocaleDateString('pt-BR', dateOptions)}`
